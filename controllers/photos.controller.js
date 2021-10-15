d4c97f920663ca6e0c56f44ff5e400ca7576cf39
Natalia Szlapka <n.machalica@gmail.com>
2021-10-15 14:32:58 +0200

    Fix bug in last commit

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -10,16 +10,17 @@ exports.add = async (req, res) => {
 
     if(title && author && email && file) { // if fields are not empty...
 
-      const fileName = file.path.split('/').slice(-1)[0];
+      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
 
-      if(
-        (fileName.path.split('.')[1] === "jpg" || fileName.path.split('.')[1] === "png" || fileName.path.split('.')[1] === "gif")){
+      if(fileName.split('.')[1] === 'jpg' || fileName.split('.')[1] === 'png' || fileName.split('.')[1] === 'gif'){
         const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
         await newPhoto.save(); // ...save new photo in DB
         res.json(newPhoto);
       } else {
         throw new Error('Wrong input!');
       }
+    } else {
+        throw new Error('Wrong input!');
     }
   }
     catch(err) {
